5c92020a1fe663ce03f8355a04cb6f89d877494a
twakelly@gmail.com <twakelly@gmail.com>
2019-11-10 23:21:30 +0000

    ...

---
+++ CMakeFiles/cw2.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/twak/code/cw2/cw2_autogen/mocs_compilation.cpp" "/home/twak/code/cw2/CMakeFiles/cw2.dir/cw2_autogen/mocs_compilation.cpp.o"
+  "/home/twak/code/cw2/responsive_label.cpp" "/home/twak/code/cw2/CMakeFiles/cw2.dir/responsive_label.cpp.o"
+  "/home/twak/code/cw2/responsive_layout.cpp" "/home/twak/code/cw2/CMakeFiles/cw2.dir/responsive_layout.cpp.o"
+  "/home/twak/code/cw2/responsive_window.cpp" "/home/twak/code/cw2/CMakeFiles/cw2.dir/responsive_window.cpp.o"
+  "/home/twak/code/cw2/screenshot.cpp" "/home/twak/code/cw2/CMakeFiles/cw2.dir/screenshot.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_MULTIMEDIAWIDGETS_LIB"
+  "QT_MULTIMEDIA_LIB"
+  "QT_NETWORK_LIB"
+  "QT_WIDGETS_LIB"
+  "QT_XML_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "cw2_autogen/include"
+  "."
+  "/home/twak/anaconda3/include/qt"
+  "/home/twak/anaconda3/include/qt/QtWidgets"
+  "/home/twak/anaconda3/include/qt/QtGui"
+  "/home/twak/anaconda3/include/qt/QtCore"
+  "/home/twak/anaconda3/./mkspecs/linux-g++"
+  "/home/twak/anaconda3/include/qt/QtMultimedia"
+  "/home/twak/anaconda3/include/qt/QtNetwork"
+  "/home/twak/anaconda3/include/qt/QtMultimediaWidgets"
+  "/home/twak/anaconda3/include/qt/QtXml"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/cw2.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cw2_autogen.dir/AutogenOldSettings.txt"
+  "CMakeFiles/cw2_autogen.dir/ParseCache.txt"
+  "cw2_autogen"
+  "CMakeFiles/cw2.dir/cw2_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/cw2.dir/responsive_label.cpp.o"
+  "CMakeFiles/cw2.dir/responsive_layout.cpp.o"
+  "CMakeFiles/cw2.dir/responsive_window.cpp.o"
+  "CMakeFiles/cw2.dir/screenshot.cpp.o"
+  "cw2"
+  "cw2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
